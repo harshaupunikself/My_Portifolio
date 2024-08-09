c161083f65a8486f05d440fb36f9bc07bf915ed4
Harsha Vardhan Reddy Bindela <harshavardhanreddy.bindela@slu.edu>
2024-08-09 12:35:29 -0400

    Harsha_Vardhan_Reddy_Bindela_Portifolio

---
+++ js/jobtitle.js
@@ -0,0 +1,13 @@
+
+const typingAnimationElement = document.getElementById('typing-animation');
+const typingTexts = ['Junior Software Developer '];
+function playTypingAnimation(text) {
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(() => { typingAnimationElement.textContent += text[i]; }, i * 200);
+    }
+    setTimeout(() => {
+        typingAnimationElement.textContent = '';
+        playTypingAnimation(typingTexts[(typingTexts.indexOf(text) + 1) % typingTexts.length]);
+    }, text.length * 200);
+}
+playTypingAnimation(typingTexts[0]);
